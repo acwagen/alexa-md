bce0b2242e825c6904ef8fb3b461ad678d245766
yiderek <yiderek@umich.edu>
2018-10-07 17:21:40 -0400

    created list display render for launch/start

--- file_system.py
+++ file_system.py
@@ -18,7 +18,69 @@ def placeholder_response(msg):
 
     return question(msg).display_render(title='Not implemented yet',  template='BodyTemplate6', background_image_url=img_url)
 
-
+def start_response(msg):
+    img_url1 = "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn"
+    img_url2 = 'https://fortunedotcom.files.wordpress.com/2017/08/512536165-e1510081190643.jpg'
+    img_url3 = 'https://akc.org/wp-content/uploads/2015/10/Beagle-Puppies.jpg'
+    item1 = {
+            "token": "List-Item-0",
+            "image": {
+              "contentDescription": "XYZ 1",
+              "sources": [
+                {
+                  "url": img_url1
+                }
+              ]
+            },
+            "textContent": {
+              "primaryText": {
+                "text": "Pupper",
+                "type": "PlainText"
+              },
+              "secondaryText": None,
+              "tertiaryText": None
+            }
+          }
+    item2 = {
+            "token": "List-Item-1",
+            "image": {
+              "contentDescription": "Sleepy Boi",
+              "sources": [
+                {
+                  "url": img_url2
+                }
+              ]
+            },
+            "textContent": {
+              "primaryText": {
+                "text": "Sleepy Boi",
+                "type": "PlainText"
+              },
+              "secondaryText": None,
+              "tertiaryText": None
+            }
+          }
+    item3 = {
+            "token": "List-Item-2",
+            "image": {
+              "contentDescription": "XYZ 3",
+              "sources": [
+                {
+                  "url": img_url3
+                }
+              ]
+            },
+            "textContent": {
+              "primaryText": {
+                "text": "Puppers",
+                "type": "PlainText"
+              },
+              "secondaryText": None,
+              "tertiaryText": None
+            }
+          }
+    my_list = [item1, item2, item3]
+    return question(msg).list_display_render(title='Welcome', template='ListTemplate2', listItems = my_list, hintText = 'Open 1')
 @ask.launch
 def launch():
     return start()
@@ -26,7 +88,7 @@ def launch():
 @ask.intent("StartIntent")
 def start():
     msg = render_template('welcome')
-    return placeholder_response(msg)
+    return start_response(msg)
     
 
 
