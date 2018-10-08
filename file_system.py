80626f81885c0237633ee9afb3a7772b8f426c39
yiderek <yiderek@umich.edu>
2018-10-08 14:52:27 -0400

    now getting images from group s3

--- file_system.py
+++ file_system.py
@@ -19,7 +19,7 @@ def open_response(msg, index):
     file_name = index + ".jpg"
     file_name = file_name.lower()
 
-    url = s3.generate_presigned_url('get_object',Params={'Bucket':'mike-alexa-md','Key':file_name,})
+    url = s3.generate_presigned_url('get_object',Params={'Bucket':'alexa-md-495','Key':file_name,})
     parts = url.split('?')
     true_url = parts[0]
 
@@ -35,12 +35,12 @@ def start_response_s3(msg):
 @ask.launch
 def launch():
     s3 = boto3.client('s3')
-    resp = s3.list_objects_v2(Bucket='mike-alexa-md')
+    resp = s3.list_objects_v2(Bucket='alexa-md-495')
     
     for obj in resp['Contents']:
         file_name = obj['Key']
         true_file_name = file_name.split('.')[0]
-        url = s3.generate_presigned_url('get_object',Params={'Bucket':'mike-alexa-md','Key':file_name,})
+        url = s3.generate_presigned_url('get_object',Params={'Bucket':'alexa-md-495','Key':file_name,})
         parts = url.split('?')
         true_url = parts[0]
         item = {
