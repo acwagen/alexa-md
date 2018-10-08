46bb27e84e1462e612267bbab00f7c2b8eebf4c4
Anthony Shinn <ayshinn@umich.edu>
2018-10-08 11:08:34 -0400

    Saying show 1 returns first dog image

--- file_system.py
+++ file_system.py
@@ -8,15 +8,30 @@ ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-def placeholder_response(msg):
-    import random
-    img_url = random.choice([
-        'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
-        'https://fortunedotcom.files.wordpress.com/2017/08/512536165-e1510081190643.jpg',
-        'https://akc.org/wp-content/uploads/2015/10/Beagle-Puppies.jpg'
-    ])
+def placeholder_response(msg, index):
+    # import random
+    # img_url = random.choice([
+    #     'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+    #     'https://fortunedotcom.files.wordpress.com/2017/08/512536165-e1510081190643.jpg',
+    #     'https://akc.org/wp-content/uploads/2015/10/Beagle-Puppies.jpg'
+    # ])
+
+    # return question(msg).display_render(title='Not implemented yet',  template='BodyTemplate6', background_image_url=img_url)
+
+    print("INDEX NUMBER IS: ", index)
+
+    if index == '1':
+        img_url = 'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn'
+    elif index == '2':
+        img_url = 'https://fortunedotcom.files.wordpress.com/2017/08/512536165-e1510081190643.jpg'
+    elif index == '3':
+        img_url = 'https://akc.org/wp-content/uploads/2015/10/Beagle-Puppies.jpg'
+    else:
+        img_url = 'https://j3uv01gyifh3iqdfjuwz0qip-wpengine.netdna-ssl.com/wp-content/uploads/2017/06/puppy.jpg'
 
     return question(msg).display_render(title='Not implemented yet',  template='BodyTemplate6', background_image_url=img_url)
+
+
 
 def start_response(msg):
     img_url1 = "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn"
@@ -81,6 +96,7 @@ def start_response(msg):
           }
     my_list = [item1, item2, item3]
     return question(msg).list_display_render(title='Welcome', template='ListTemplate2', listItems = my_list, hintText = 'Open 1')
+
 @ask.launch
 def launch():
     return start()
@@ -92,12 +108,12 @@ def start():
     
 
 
-@ask.intent("OpenIntent")
-def open():
+@ask.intent("OpenIntent", mapping={'imageIndex': 'imageIndex'})
+def open(imageIndex):
     filename = 'filename'
     open_msg = render_template('open', filename=filename)
 
-    return placeholder_response(open_msg)
+    return placeholder_response(open_msg, imageIndex)
 
 
 @ask.intent("HelpIntent")
