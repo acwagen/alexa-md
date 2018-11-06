3e52f2d596da3a28d1569ffaa52851adb54f45db
Alyssa Wagenmaker <acwagen@umich.edu>
2018-11-06 17:10:44 -0500

    Error handling and UI updates

--- alexa-md-upload/alexamd_upload/views/views.py
+++ alexa-md-upload/alexamd_upload/views/views.py
@@ -1,5 +1,5 @@
 from alexamd_upload import app, model
-from flask import render_template, request, session
+from flask import render_template, request, session, flash
 from s3utils import s3upload, s3delete
 import uuid
 import png
@@ -8,9 +8,9 @@ import pydicom
 
 def get_context():
     context = {}
-    if 'patient_id' in session:
+
+    if 'patient_id' in session and 'patient_name' in session:
         context['patient_id'] = session['patient_id']
-    if 'patient_name' in session:
         context['patient_name'] = session['patient_name']
 
     return context
@@ -20,7 +20,8 @@ def index():
     db = model.get_db()
 
     if request.method == 'POST':
-        session['patient_id'] = request.form['patient']
+        # TODO: session timeout
+        session['patient_id'] = int(request.form['patient'])
 
         patient = db.execute('select p_first, p_last from patients where pid = ?',
             (session['patient_id'],)).fetchone()
@@ -30,7 +31,8 @@ def index():
     context['patients'] = []
 
     for patient in db.execute('select pid, p_first, p_last from patients'):
-        context['patients'].append({'id': patient['PID'], 'name': '{}, {}'.format(patient['P_Last'], patient['P_First'])})
+        context['patients'].append({'id': patient['PID'],
+            'name': '{}, {}'.format(patient['P_Last'], patient['P_First'])})
 
     return render_template('index.html', **context)
 
@@ -44,13 +46,15 @@ def upload(patient_id):
 
         # collection_id < 0 means a new collection should be created
         if collection_id < 0:
-            print('Creating new collection with name {}'.format(request.form['new_collection']))
+            app.logger.info('Creating new collection with name {}'.format(
+                request.form['new_collection']))
             db.execute('insert into collections(c_name, study, pid) values (?, ?, ?)',
             (request.form['new_collection'], 'Other', session['patient_id']))
-            collection_id = db.execute('select max(cid) as new_cid from collections').fetchone()['new_cid']
 
+            collection_id = db.execute('select max(cid) as new_cid from collections'
+                ).fetchone()['new_cid']
 
-        print('Adding image(s) to collection with id  {}'.format(collection_id))
+        app.logger.info('Adding image(s) to collection with id  {}'.format(collection_id))
 
         study = db.execute('select study from collections where cid = ?',
                            (collection_id,)).fetchone()['Study']
@@ -59,6 +63,7 @@ def upload(patient_id):
         if not cur_idx:
             cur_idx = 0
 
+        # TODO: add progress bar to show how many images are processed
         for file in request.files.getlist('files'):
             image_id = str(uuid.uuid1())
             db.execute('insert into images(iid, cid, ind) values (?,?,?)',
@@ -69,15 +74,13 @@ def upload(patient_id):
             try:
                 ds = pydicom.dcmread(file)
             except pydicom.errors.InvalidDicomError:
-                print('Error: invalid dicom')
-                # TODO: alert user of error
+                flash('Error: Invalid dicom: {}'.format(file.filename))
                 continue
 
             if study == 'Other':
                 study = ds.Modality
             elif study != ds.Modality:
-                # TODO: alert user or error
-                print('Error: images in collection are of different modalities')
+                flash('Error: Image {} has a different modality than selected sequence'.format(file.filename))
 
             # Convert to float to avoid overflow or underflow losses.
             image_2d = ds.pixel_array.astype(float)
@@ -113,24 +116,28 @@ def manage_patient(patient_id):
 
     if request.method == 'POST':
         if 'id' in request.form:
-            print('Deleting collection with id {}'.format(request.form['id']))
+            app.logger.info('Deleting collection with id {}'.format(request.form['id']))
             for image in db.execute('select i.iid from images i where cid = ?',
                 (request.form['id'],)):
-                s3delete(image['iid'])
+                s3delete(image['IID'])
 
-            # TODO: add on delete cascade so all of the collection's images will be deleted
             db.execute('delete from collections where cid = ?', (request.form['id'],))
         else:
-            print('Creating new collection with name {}'.format(request.form['name']))
+            app.logger.info('Creating new collection with name {}'.format(request.form['name']))
             db.execute('insert into collections(c_name, study, pid) values (?, ?, ?)',
                        (request.form['name'], 'Other', session['patient_id']))
 
     context = get_context()
 
     context['items'] = []
-    for collection in db.execute('select cid, c_name, study from collections where pid = ?',
+    for collection in db.execute('select c.cid, c.c_name, c.study \
+         from collections c where c.pid = ?',
                                  (session['patient_id'],)):
-        context['items'].append({'id': collection['CID'], 'name': collection['C_Name']})
+        # this might be able to be combined into the above query
+        count = db.execute('select count(*) as count from images where cid = ?',
+            (collection['CID'],)).fetchone()['count']
+        context['items'].append({'id': collection['CID'],
+            'name': '{} ({} images)'.format(collection['C_Name'], count) })
 
     return render_template('manage.html', **context)
 
@@ -140,24 +147,29 @@ def manage():
 
     if request.method == 'POST':
         if 'id' in request.form:
-            print('Deleting patient with id {}'.format(request.form['id']))
-            # TODO: test this query
-            for image in db.execute('select i.iid from images i join collections c on c.cid = i.cid where c.pid = ?',
+            app.logger.info('Deleting patient with id {}'.format(request.form['id']))
+            for image in db.execute('select i.iid from images i \
+                join collections c on c.cid = i.cid where c.pid = ?',
                 (request.form['id'],)):
                 s3delete(image['IID'])
 
-            # TODO: add on delete cascade so all of the patient's collections and images will be deleted
             db.execute('delete from patients where pid = ?', (request.form['id'],))
+
+            if 'patient_id' in session and session['patient_id'] == int(request.form['id']):
+                session.pop('patient_id')
+                session.pop('patient_name')
         else:
-            print('Adding patient with name {}'.format(request.form['name']))
+            app.logger.info('Adding patient with name {}'.format(request.form['name']))
             name = request.form['name'].split()
             db.execute('insert into patients(p_first, p_last) values (?, ?)',
                        (name[0], name[1],))
+
 
     context = get_context()
     context['items'] = []
 
     for patient in db.execute('select pid, p_first, p_last from patients'):
-        context['items'].append({'id': patient['PID'], 'name': '{} {}'.format(patient['P_First'], patient['P_Last'])})
+        context['items'].append({'id': patient['PID'],
+            'name': '{} {}'.format(patient['P_First'], patient['P_Last'])})
 
     return render_template('manage.html', **context)
