19acb96ad300bce498ee7740ea1bccade6fd0890
Alyssa Wagenmaker <acwagen@umich.edu>
2018-12-03 14:10:01 -0500

    Add basic tests for open at each level

--- alexa-md/alexamd/tests/test_patient.py
+++ alexa-md/alexamd/tests/test_patient.py
@@ -6,7 +6,7 @@ import os
 
 class TestOpenPatient(AlexaMDTestBase):
 
-    def test_open_patient(self):
+    def test_default(self):
         response = self.app.post('/', data=self.openIntent('1'))
         self.assertEqual(200, response.status_code)
 
@@ -17,7 +17,7 @@ class TestOpenPatient(AlexaMDTestBase):
         self.assertListEqual(list_items, ['CT', 'MRI'])
         self.assertDictEqual(session_attributes, {'patient': '1', 'level': 'patient'})
 
-    def test_open_empty_patient(self):
+    def test_empty(self):
         self._execute_sql_command("insert into patients(p_first, p_last) values ('test', 'patient')")
 
         response = self.app.post('/', data=self.openIntent('5'))
@@ -28,4 +28,18 @@ class TestOpenPatient(AlexaMDTestBase):
         session_attributes = self.getSessionAttributesFromResponse(response_data)
 
         self.assertListEqual(list_items, [])
+        self.assertDictEqual(session_attributes, {'patient': '5', 'level': 'patient'})
+
+    def test_empty_study(self):
+        self._execute_sql_command("insert into patients(p_first, p_last) values ('test', 'patient')")
+        self._execute_sql_command("insert into collections(c_name, study, pid) values ('test', 'CT', 5)")
+
+        response = self.app.post('/', data=self.openIntent('5'))
+        self.assertEqual(200, response.status_code)
+
+        response_data = json.loads(response.data)
+        list_items = self.getListItemsFromResponse(response_data)
+        session_attributes = self.getSessionAttributesFromResponse(response_data)
+
+        self.assertListEqual(list_items, ['CT'])
         self.assertDictEqual(session_attributes, {'patient': '5', 'level': 'patient'})
